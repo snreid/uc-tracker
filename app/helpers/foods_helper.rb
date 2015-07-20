c4688893a67ba8dd8196fc80e120a791e8a6f00e
Sarah <reid.419@osu.edu>
2015-07-20 07:56:16 -0400

    Fail gracefully when viewing nutrition information.

--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -14,33 +14,46 @@ module FoodsHelper
           end  
         end
       else
-        haml_tag "ul" do
-          haml_tag "li" do
-            haml_concat("Serving Size: " + food.serving_size.to_s) 
-          end
-          haml_tag "li" do
-            haml_concat("Calories: " + food.calories.to_s) 
-          end
-          haml_tag "li" do
-           haml_concat("Fat: " + food.fat.to_s)
-          end
-          haml_tag "li" do
-            haml_concat("Carbohydrates: " + food.carbohydrates.to_s) 
-          end
-          haml_tag "li" do
-           haml_concat("Protein: "+ food.protein.to_s)
-          end
-          haml_tag "li" do 
-            haml_concat("Fiber: " + food.fiber.to_s) 
-          end
-          haml_tag "li" do 
-            haml_concat("Sodium: " + food.sodium.to_s) 
-          end
-          haml_tag "li" do 
-            haml_concat("Sugars: " + food.sugars.to_s) 
-          end
-        end
+        output_stored_nutrition_facts(food)
+      end
+
+    end
+  rescue
+    capture_haml do
+      haml_tag "h5" do
+        haml_concat "The USDA API is not available at this time. You may not be connected to the internet."
+      end
+      output_stored_nutrition_facts(food)
+    end
+  end
+
+  def output_stored_nutrition_facts(food)
+    haml_tag "ul" do
+      haml_tag "li" do
+        haml_concat("Serving Size: " + food.serving_size.to_s) 
+      end
+      haml_tag "li" do
+        haml_concat("Calories: " + food.calories.to_s) 
+      end
+      haml_tag "li" do
+       haml_concat("Fat: " + food.fat.to_s)
+      end
+      haml_tag "li" do
+        haml_concat("Carbohydrates: " + food.carbohydrates.to_s) 
+      end
+      haml_tag "li" do
+       haml_concat("Protein: "+ food.protein.to_s)
+      end
+      haml_tag "li" do 
+        haml_concat("Fiber: " + food.fiber.to_s) 
+      end
+      haml_tag "li" do 
+        haml_concat("Sodium: " + food.sodium.to_s) 
+      end
+      haml_tag "li" do 
+        haml_concat("Sugars: " + food.sugars.to_s) 
       end
     end
   end
 end
+
