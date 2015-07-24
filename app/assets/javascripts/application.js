28b22ae8ed4ab0b30518e187ae85cea4999d5064
Sarah <reid.419@osu.edu>
2015-07-24 11:32:40 -0400

    Rough jump-start to HighChart.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,12 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
-//= require turbolinks
-//= require jquery
-//= require bootstrap-sprockets
-//= require bootstrap/modal
 //= require highcharts
-//= require highcharts/highcharts-more # to get the new features in 2.3.0
+//= require highcharts/highcharts-more
 //= require highcharts/adapters/mootools-adapter
 //= require highcharts/adapters/prototype-adapter
 //= require highcharts/adapters/standalone-framework
@@ -30,8 +26,9 @@
 //= require highcharts/modules/funnel
 //= require highcharts/modules/heatmap
 //= require highcharts/modules/no-data-to-display
-
 //= require highcharts/themes/skies
+//= require bootstrap-sprockets
+//= require bootstrap/modal
 //= require_tree .
 
 
@@ -56,11 +53,53 @@ $(document).ready(function(){
     return false; // prevents normal behaviour
   });
   
-  // $('.trackable-modal-form').submit(function() {  
-  //   capture_submit_to_ajax(this, "Thank you for tracking!")
-  //   return false; // prevents normal behaviour
-  // });
 
+  $('#chart-container').highcharts({
+        title: {
+            text: 'Monthly Average Temperature',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }, {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }]
+    });
 
 
 });
